15e67868f1729ae952d10b08a4a62c2e3d7dff4a
Pavan Balaji Kumar <pavanbalaji22@gmail.com>
2026-06-10 17:44:41 -0400

    feat:user input

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "agoi-code/api"
+import (
+	"agoi-code/llm"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	api.Serve()
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Hello! Welcome to agoi code")
+	fmt.Println(strings.Repeat("-", 30))
+	fmt.Println("Please chose a model ")
+	fmt.Printf(":> ")
+	model, err := reader.ReadString('\n')
+	llmClient := llm.NewOllamaClient(model)
+	checkError(err)
+	for {
+		fmt.Printf(":> ")
+		input, err := reader.ReadString('\n')
+		checkError(err)
+		res, err := llmClient.CallLLM(input)
+		checkError(err)
+		fmt.Printf("%s : %s \n", strings.Replace(model, "\n", "", 1), res)
+
+	}
+
 }
